be775679dec183e5d56b004ea789da7eb9730e36
Mayank aneja <anejamayank16@gmail.com>
2025-06-02 18:41:40 +0530

    added AppBar and modifed button

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,9 +1,13 @@
+"use client"
+import { signIn, signOut, useSession } from "next-auth/react";
+import { AppBar } from "@repo/ui/appbar";
 import { JSX } from "react";
 
-export default function Page():JSX.Element {
+export default function Page(): JSX.Element {
+  const session = useSession();
   return (
-    <div className="text-2xl font-bold">
-      Hello whats up motherfucker
-    </div>
-  )
+   <div>
+      <AppBar onSignin={signIn} onSignout={signOut} user={session.data?.user} />
+   </div>
+  );
 }
---
+++ packages/ui/src/AppBar.tsx
@@ -0,0 +1,21 @@
+import { Button } from "./button"
+interface AppBarProps{
+    user?:{
+        name?:string | null
+    }
+    onSignin:any,
+    onSignout:any
+}
+export const AppBar=({user,onSignin,onSignout}:AppBarProps)=>{
+    return <div className="flex justify-between border-b px-4">
+        <div className="text-lg flex flex-col justify-center">
+            PayTM
+        </div>
+        <div className="flex flex-col justify-around font-extrabold">
+            This is a tailwind Test
+        </div>
+        <div className="flex flex-col justify-center pt-2">
+            <Button onClick={user? onSignout: onSignin}>{user ? "Logout":"Log-In"}</Button>
+        </div>
+    </div>
+}
