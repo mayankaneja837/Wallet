46aa7dcb59d7939b36c718555f2f65862a6b094c
Mayank aneja <anejamayank16@gmail.com>
2025-06-03 00:26:45 +0530

    added a seed script

--- packages/ui/src/AppBar.tsx
+++ packages/ui/src/AppBar.tsx
@@ -8,13 +8,13 @@ interface AppBarProps{
 }
 export const AppBar=({user,onSignin,onSignout}:AppBarProps)=>{
     return <div className="flex justify-between border-b px-4">
-        <div className="text-lg flex flex-col justify-center">
+        <div className="text-lg flex flex-col justify-center pl-2 cursor-pointer">
             PayTM
         </div>
         <div className="flex flex-col justify-around font-extrabold">
             This is a tailwind Test
         </div>
-        <div className="flex flex-col justify-center pt-2">
+        <div className="flex flex-col justify-center pt-2 cursor-pointer pl-2">
             <Button onClick={user? onSignout: onSignin}>{user ? "Logout":"Log-In"}</Button>
         </div>
     </div>
